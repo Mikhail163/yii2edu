8a297b4aeead407d7c59d18dc6ecdb7eeee3ecc6
Mikhail <mephist0_87@mail.ru>
2018-10-11 09:13:41 +0000

    migration dont work? why??

--- migrations/m181011_085753_add_foreign_key_user_task.php
+++ migrations/m181011_085753_add_foreign_key_user_task.php
@@ -12,10 +12,10 @@ class m181011_085753_add_foreign_key_user_task extends Migration
      */
     public function safeUp()
     {
-    	$this->addForeignKey('fx_taskuser_user', 'task_user', ['user_id'], 'user', ['id']);
-    	$this->addForeignKey('fx_taskuser_task', 'task_user', ['task_id'], 'task', ['id']);
-    	$this->addForeignKey('fx_task_user1', 'task', ['creator_id'], 'user', ['id']);
-    	$this->addForeignKey('fx_task_user2', 'task', ['updater_id'], 'user', ['id']);
+    	$this->addForeignKey('fx_taskuser_user', 'task_user', ['user_id'], 'user', ['user_id']);
+    	$this->addForeignKey('fx_taskuser_task', 'task_user', ['task_id'], 'task', ['task_id']);
+    	$this->addForeignKey('fx_task_user1', 'task', ['creator_id'], 'user', ['user_id']);
+    	$this->addForeignKey('fx_task_user2', 'task', ['updater_id'], 'user', ['user_id']);
     }
 
     /**
