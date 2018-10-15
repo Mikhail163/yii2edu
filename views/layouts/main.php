552a682dce8340c697e48966c4ae0ce78ef3de6c
Mikhail <mephist0_87@mail.ru>
2018-10-15 12:38:44 +0000

    task 5 repear UserBase -> User

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -42,7 +42,8 @@ AppAsset::register($this);
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
         	['label' => 'Test', 'url' => ['/test/index']],
-        	['label' => 'Бренды', 'url' => ['/brend/index']],
+        	['label' => 'Пользователи', 'url' => ['/user/index']],
+        	['label' => 'Задачи', 'url' => ['/task/index']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
